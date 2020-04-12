16e665fc9199242b5cb3c60f82e9f60b78faa554
Luís Eduardo Pompeu <56649205+luiseduardobr1@users.noreply.github.com>
2020-04-12 15:54:57 -0300

    Add files via upload

---
+++ algoritmos/atualizar_google_sheets_brasilio.py
@@ -0,0 +1,54 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import datetime
+from openpyxl import load_workbook
+import re, requests, time
+from bs4 import BeautifulSoup
+
+# Entrar no readme
+response = requests.get('https://github.com/luiseduardobr1/covid19-Ceara/blob/master/README.md')
+raw_data = response.text
+
+# URL do último arquivo municípios
+URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'\".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+link_municipio = re.findall(URL_REGEX, raw_data)[72]
+fim_link = link_municipio.find(r'/municipios_')+1
+link = 'https://raw.githubusercontent.com/luiseduardobr1/covid19-Ceara/master/integraSUS_Municipios_CE/' + link_municipio[fim_link:]
+
+# Ler CSV municipios mais atualizado
+mun = pd.read_csv(link)
+
+# Data hoje e ontem
+date = datetime.datetime.now()
+
+# Novo dataframe
+novo = mun[mun['Data']==str(date.date())]
+novo = novo.sort_values(by=['municipio', 'Data']).reset_index()
+
+#  GOOGLE SHEETS
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('JSON HERE', scope)
+gc = gspread.authorize(credentials)
+wks = gc.open_by_key('URL KEY HERE')
+
+# Primeira Sheet
+worksheet = wks.get_worksheet(0)
+
+#Atualiza celula
+#worksheet.update_acell('B5', 'Funciona :D')
+
+# Extrai valor de uma celula
+#worksheet.cell(5,2).value
+
+# Valores de uma linha
+#val = worksheet.row_values(1)
+
+# Valores de uma coluna
+#val = worksheet.col_values(1)
+
+for i in range(0,len(novo['municipio'])):
+    cell = worksheet.find(novo['municipio'][i])
+    worksheet.update_cell(cell.row, 2, str(novo['qtdConfirmado'][i]))
+    worksheet.update_cell(cell.row, 3, str(novo['Obitos'][i]))
+    time.sleep(2) #somente por causa da limitação de 100 requests por 100 s para um mesmo projeto e usuário
